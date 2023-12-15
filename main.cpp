f3a3dcae0ad9abc0475b906bcc46314e036f51fe
IFTE-13 <ifte.phoenix@gmail.com>
2023-12-15 23:03:39 +0600

    Initializing

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <GL/freeglut.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <vector>
+#include <math.h>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_MULTISAMPLE);
+    glutInitWindowPosition(0, 0);
+    glutInitWindowSize(1920, 1080);
+    glutFullScreen();
+    glutMainLoop();
+}
