ca28498989db2c0c32e685138f020aa49043fa15
IFTE-13 <ifte.phoenix@gmail.com>
2023-12-15 23:27:03 +0600

    Shapes

--- main.cpp
+++ main.cpp
@@ -25,6 +25,56 @@ Color interpolateColor(Color color1, Color color2, float t) {
     return Color(r, g, b);
 }
 
+/// Shapes Structure
+void quad(float x1, float x2, float y1, float y2, Color color, float Tx = 0, float Ty = 0, float s = 1)
+{
+    glColor3ub(color.r, color.g, color.b);
+    glBegin(GL_QUADS);
+    glVertex2f(s * x1 + Tx, s * y1 + Ty);
+    glVertex2f(s * x2 + Tx, s * y1 + Ty);
+    glVertex2f(s * x2 + Tx, s * y2 + Ty);
+    glVertex2f(s * x1 + Tx, s * y2 + Ty);
+    glEnd();
+}
+
+void circle(float x, float y, float radius, float height, Color color)
+{
+    int triangleAmount = 360;
+    glBegin(GL_TRIANGLE_FAN);
+    glColor3ub(color.r, color.g, color.b);
+    glVertex2f(x, y); // center of circle
+    for (int i = 0; i <= 360; i++)
+        glVertex2f(x + (radius * cos(i * 2 * 3.1416 / triangleAmount)), y + (height * sin(i * 2 * 3.1416 / triangleAmount)));
+    glEnd();
+}
+
+void quad(vector<pair<float, float>> coord, Color color = {255, 255, 255}, float Tx = 0, float Ty = 0, float s = 1)
+{
+    glColor3ub(color.r, color.g, color.b);
+    glBegin(GL_QUADS);
+    for (int i = 0; i < coord.size(); i++)
+        glVertex2d(Tx + s * coord[i].first, Ty + s * coord[i].second);
+    glEnd();
+}
+
+void triangle(vector<pair<float, float>> coord, Color color = {255, 255, 255}, float Tx = 0, float Ty = 0, float s = 1)
+{
+    glColor3ub(color.r, color.g, color.b);
+    glBegin(GL_TRIANGLES);
+    for (int i = 0; i < coord.size(); i++)
+        glVertex2d(Tx + s * coord[i].first, Ty + s * coord[i].second);
+    glEnd();
+}
+
+void polygon(vector<pair<float, float>> coord, Color color = {255, 255, 255}, float Tx = 0, float Ty = 0, float s = 1)
+{
+    glColor3ub(color.r, color.g, color.b);
+    glBegin(GL_POLYGON);
+    for (int i = 0; i < coord.size(); i++)
+        glVertex2d(Tx + s * coord[i].first, Ty + s * coord[i].second);
+    glEnd();
+}
+
 void init(void)
 {
     glClearColor(0.0F, 0.0F, 0.0F, 1);
