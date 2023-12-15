943f2c28286217ca6b00a665cbed39bf7d75d338
IFTE-13 <ifte.phoenix@gmail.com>
2023-12-15 23:26:09 +0600

    Color and Gradient Structure

--- main.cpp
+++ main.cpp
@@ -7,6 +7,32 @@
 
 using namespace std;
 
+/// Defining Color for Objects
+struct Color
+{
+    int r;
+    int g;
+    int b;
+
+    Color(int red, int green, int blue) : r(red), g(green), b(blue) {}
+};
+
+/// Generating Gradient Colors
+Color interpolateColor(Color color1, Color color2, float t) {
+    int r = static_cast<int>((1 - t) * color1.r + t * color2.r);
+    int g = static_cast<int>((1 - t) * color1.g + t * color2.g);
+    int b = static_cast<int>((1 - t) * color1.b + t * color2.b);
+    return Color(r, g, b);
+}
+
+void init(void)
+{
+    glClearColor(0.0F, 0.0F, 0.0F, 1);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0, 1920, 0, 1080, -100, 100);
+}
+
 int main(int argc, char **argv)
 {
     glutInit(&argc, argv);
@@ -14,5 +40,6 @@ int main(int argc, char **argv)
     glutInitWindowPosition(0, 0);
     glutInitWindowSize(1920, 1080);
     glutFullScreen();
+    init();
     glutMainLoop();
 }
